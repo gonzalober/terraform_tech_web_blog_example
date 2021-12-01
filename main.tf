74aa7778acb4434817ab58055474ffe75738b997
jonathan <jonathan.vickers@ecs.co.uk>
2021-12-01 09:50:51 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = var.region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
---
+++ securitygroup.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "allow_http" {
+  name        = "${var.name}_allow_http"
+  description = "Allow http inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "http access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name  = "${var.name}_allow_http"
+    Owner = var.name
+  }
+}
